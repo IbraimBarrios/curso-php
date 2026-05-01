10b4879c6676ce695c8d405ff5913a7c258503f7
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-01 13:47:43 -0600

    Add Hello World

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+print "Hello World";
+echo "Hello Wold", "Hello World 2";
+?>
+
+
+<!-- Nota: etiquetas obsoletas. en condigo viejo se puede encontrar -->
+<!-- <script language="php" >
+
+</script>
+
+<%
+
+%> -->
