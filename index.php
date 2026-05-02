6b1e236604a43214e9a3f32438cb24d1b915051e
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-02 11:24:52 -0600

    Variables, Constantes y Tipos de datos

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php 
-print "Hello World";
-echo "Hello Wold", "Hello World 2";
+// Nota: las variables constantes no se pueden re-asignar
+//constantes
+const escuela = "Niños Heroes";
+define("direccion","avenida alegria");
+//escuela = "Escuela"; 
+
+
+//variables y tipos de datos
+$name = "Hello Wold";
+$edad = 10;
+$promedio = 9.2;
+$is_alumno = false;
+$nombre = 10;
+$nombre = "Jose";
+
+echo $nombre, " tiene: ", $edad, " años"," de la escuela: ", escuela, " ", direccion;
 ?>
-
-
-<!-- Nota: etiquetas obsoletas. en condigo viejo se puede encontrar -->
-<!-- <script language="php" >
-
-</script>
-
-<%
-
-%> -->
