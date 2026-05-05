644ea398564892863a77fb6e5ddc07364fd6b7cd
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-04 18:22:25 -0600

    Operadores de comparación

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
 <?php 
-$RFC = "91 75 1A EC C7";
-$buscar = " ";
-$remplazar = ":";
+//Operadores de comparación
+// Operador igual ==
+// Operador identico ===
+// Operador diferente !=
+// Operador diferente <>
+// Operador no identico !== 
+$raza = "Husky";
+$edad = "10";
+$edad2 = 20;
 
-$resultado = str_replace($buscar, $remplazar, $RFC);
-echo $resultado;
+var_dump($raza == "Akita");
+var_dump($edad === 10);
+var_dump($edad2 != 10);
+var_dump($edad2 <> 10);
+var_dump($edad2 !== "28");
 
 
-$anio = "1994";
-$mes = "01";
-$dia = "21";
-$formato = "%s-%s-%s";
+// Operador Mayor que >
+// Operador Mayor o igual que >=
+// Operador Menor que <
+// Operador Menor o igual que <=
+$edad3 = 18;
+$edad4 = 6;
 
-echo sprintf($formato, $dia, $mes, $anio);
+var_dump($edad3 > 18);
+var_dump($edad3 >= 18);
+var_dump($edad4 < 6);
+var_dump($edad4 <= 6);
+
+/* Operador de nave espacial <=>
+0 Ambos valores son iguales
+-1 El valor es menor
+1 El valor es mayor
+*/
+
+$edad5 = 6;
+$edad6 = 3;
+$edad7 = 7;
+var_dump($edad5 <=> 6);
+var_dump($edad6 <=> 6);
+var_dump($edad7 <=> 6);
 
 ?>
