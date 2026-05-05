6ee188634c0bb80bebdd590ec492286f9062cf13
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-05 12:50:02 -0600

    comentario

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $edad = 20;
 $trae_credencial = "No";
 
 // Nota: cuando se aga una comparacion y tenga Operadores de asignacion ==, <= entre otros
-// es recomedable usar && por que tienen mas jerarquia que el and
+// es recomedable usar && o || por que tienen mas jerarquia que el and o or
 $puede_votar = $edad >= 18 && $trae_credencial == "Si";
 
 var_dump($puede_votar);
