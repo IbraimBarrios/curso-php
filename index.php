42966107dba3ef1942efb3beea2bfb35d4716ba2
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-07 10:16:29 -0600

    do while

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 // while
+// do while
 
 $zapatos = 0;
 
@@ -7,5 +8,12 @@ while ($zapatos <= 100) {
   echo "Zapato Numero: ", $zapatos;
   $zapatos++;
 }
+
+echo $zapatos;
+
+do {
+  echo "Zapato Numero: ", $zapatos;
+  $zapatos++;
+} while ($zapatos <= 103);
 
 ?>
