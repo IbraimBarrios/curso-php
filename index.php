4e2200d832ca1e17f31a6c92c345066f98b67581
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-12 09:51:11 -0600

    Parámetros y argumentos

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
-// funciones
+// funciones: Parámetros y argumentos 
 
-function obtener_edad(){
-  $edad = 2026 - 1996;
+// $anio = 1996;
+function obtener_edad($anio, $anio_actual = 2026){
+  $edad = $anio_actual - $anio;
   echo "Mi edad es: $edad";
 }
 
-obtener_edad();
+obtener_edad(1996, 2010);
+obtener_edad(2000);
 
 ?>
