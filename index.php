fd7ed49e2ad61ea3dd33054839ffe13f479e4e24
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-14 17:53:44 -0600

    Remover Cometarios

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php 
-// cargar las rutas, de las instancias que se utilizan
-// en este caso se utilizan 3: Cliente, Empleado, Proveedor.
 require_once("autoload.php");
 (new AutoCarga())->cargarRutas();
 
