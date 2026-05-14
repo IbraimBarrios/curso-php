46b7b760fc90f14d941c39c5f2a0114bb71bdbb7
Ibraim Barrios <barrios.party@hotmail.com>
2026-05-14 12:16:00 -0600

    Type Hinting

--- cliente.php
+++ cliente.php
@@ -1,5 +1,6 @@
 <?php
 require_once("persona.php");
+require_once("empleado.php");
 class Cliente extends Persona {
   public function __construct(
     public bool $frecuente = false,
@@ -9,6 +10,10 @@ class Cliente extends Persona {
   public function comer(){
     $telefono = $this->getTelefono();
     echo $this->validarTelefono($telefono);
+  }
+
+  function saludar(Empleado $empleado) {
+    echo "Felicidades chef ", $empleado->nombre;
   }
 } 
 ?>
